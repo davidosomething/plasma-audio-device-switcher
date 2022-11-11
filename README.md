53b387127763f3780215c90371d5de3c01fefe7d
David O'Trakoun <me@davidosomething.com>
2022-11-10 20:59:53 -0500

    merge outstanding prs, rename, repack

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# plasma-audio-device-switcher
+# plasma-audio-device-switcher (davidosomething fork)
+
+- Merged all the outstanding PRs 2022-11-10
+- Convert metadata.desktop to metadata.json
+- Rename identifiers
+- Repack for installation from local file
 
 Simple widget to change the default audio output device/sink.
--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@
 
 cd package
 
-version=$(sed -n 's/^X-KDE-PluginInfo-Version=//p' metadata.desktop)
-zip -R "../audio-device-switcher-v$version.plasmoid" '*.qml' '*.desktop' '*.xml'
+version=$(sed -n 's/^X-KDE-PluginInfo-Version=//p' metadata.json)
+zip -R "../audio-device-switcher-v$version.plasmoid" '*.qml' '*.json' '*.xml'
 
 cd ..
